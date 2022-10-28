a77b446269d56af688ec296190a71c9c949b6e9a
Matheus Mello <matheus.mello@getarive.com>
2022-10-28 17:11:17 +0100

    Added README.MD instructions

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App() {
             <label htmlFor="quantity" className="block mb-2 text-sm font-medium text-gray-900 text-gray-300">Quantity</label>
             <input className='mx-auto w-full' disabled={product === undefined} type="range" min={1} step={1} value={quantity} onChange={(e) => setQuantity(+e.target.value)} max={product?.maxAmount} />
             <div className={`grid ${product && 'grid-cols-4'}`}>
-              <input type="number" value={quantity} id="quantity" disabled={product === undefined} onChange={(e) => setQuantity(+e.target.value)} className="mx-auto w-16 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="" required />
+              <input type="number" value={quantity} id="quantity" disabled={product === undefined} onChange={(e) => setQuantity(+e.target.value)} className="mx-auto w-16 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white" placeholder="" required />
               {product && (
                 <div className='grid grid-cols-6 col-span-3'>
                   <span className='text-white text-center p-0 m-0 pt-1 font-bold'>x</span>
@@ -69,7 +69,7 @@ function App() {
           </div>
 
           <div className='grid self-center mt-10 sm:mt-0'>
-            <button type="button" disabled={product === undefined} onClick={addToShoppingCart} className={`mx-auto w-44 h-12 px-4 rounded-full text-white bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-blue-800 ${undefinedProductDisabledStyle}`}>Add to Cart</button>
+            <button type="button" disabled={product === undefined} onClick={addToShoppingCart} className={`mx-auto w-44 h-12 px-4 rounded-full text-white bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 hover:bg-blue-400 focus:outline-none focus:ring-blue-800 ${undefinedProductDisabledStyle}`}>Add to Cart</button>
           </div>
         </div>
 
